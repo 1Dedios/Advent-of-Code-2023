d7f00eccd78caabaee416187278ae4f5b404f930
Dedios <dediosprogramming@gmail.com>
2023-12-01 18:45:50 -0500

    calibrationValues implemented successfully for numbers

--- Day1/calibrationValues.js
+++ Day1/calibrationValues.js
@@ -5,15 +5,41 @@ const calibrationValues = async () => {
     const data = await fs.readFile('./Day1/input.txt', { encoding: 'utf8' });
     const splitDataByLine = data.split('\n');
     const extractedNumbersFromData = [];
+    const firstAndLastNumberFromExtractedNumbers = [];
 
     splitDataByLine.forEach((line) => {
       const numberMatch = /[0-9+]/g;
       const extractNumbers = line.match(numberMatch);
       const formatExtractedNumber = extractNumbers.join('');
-      extractedNumbersFromData.push(parseInt(formatExtractedNumber));
+      console.log(formatExtractedNumber);
+      extractedNumbersFromData.push(formatExtractedNumber);
     });
 
-    console.log(extractedNumbersFromData.reduce((acc, val) => acc + val, 0));
+    console.log(extractedNumbersFromData);
+
+    extractedNumbersFromData.forEach((element) => {
+      if (element.length === 1) {
+        firstAndLastNumberFromExtractedNumbers.push(element * 11);
+      } else if (element.length > 2) {
+        let firstDigit = element.charAt(0);
+        console.log(firstDigit);
+        let lastDigit = element.charAt(element.length - 1);
+        console.log(lastDigit);
+        firstAndLastNumberFromExtractedNumbers.push(
+          parseInt(firstDigit.concat(lastDigit))
+        );
+      } else {
+        firstAndLastNumberFromExtractedNumbers.push(parseInt(element));
+      }
+    });
+
+    console.log(
+      firstAndLastNumberFromExtractedNumbers.reduce((acc, val) => acc + val, 0)
+    );
+    return firstAndLastNumberFromExtractedNumbers.reduce(
+      (acc, val) => acc + val,
+      0
+    );
   } catch (err) {
     console.error(err);
   }
