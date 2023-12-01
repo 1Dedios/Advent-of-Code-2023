dc4817d45f79da0ba4a6a1d9851f3eab85d0bdee
Dedios <dediosprogramming@gmail.com>
2023-12-01 00:59:34 -0500

    text-file-parser module created

---
+++ Day1/text-file-parser.js
@@ -0,0 +1,15 @@
+/**
+ * 
+PLAN: 
+
+- FileReader API - reads contents of a file as text. 
+- The contents of the file are lines of text
+- Read through each line of text and take the first number in the sequence and the last number in the sequence.
+- Concat the first and last numbers
+- Push the concacted numbers to an array
+- move to the next line and do the same
+- once you've reached the end of the file
+- reduce the array of the concacted numbers to a sum
+- return this sum. 
+
+ */
