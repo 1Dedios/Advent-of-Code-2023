cb0a5a7b00d8fc4928c0845e1f46aa251c50dd2d
Dedios <dediosprogramming@gmail.com>
2023-12-03 17:24:13 -0500

    refactored calibrationValues.js to use string.replace()

--- Day1/calibrationValues.js
+++ Day1/calibrationValues.js
@@ -2,18 +2,35 @@ const fs = require('node:fs/promises');
 
 const calibrationValues = async () => {
   try {
-    const data = await fs.readFile('./Day1/input.txt', { encoding: 'utf8' });
+    const data = await fs.readFile('./Day1/sample-input.txt', {
+      encoding: 'utf8',
+    });
     const splitDataByLine = data.split('\n');
+    console.log(splitDataByLine);
     const extractedNumbersFromData = [];
     const firstAndLastNumberFromExtractedNumbers = [];
 
     splitDataByLine.forEach((line) => {
-      const numberMatch = /[0-9+]/g;
-      const extractNumbers = line.match(numberMatch);
-      const formatExtractedNumber = extractNumbers.join('');
-      console.log(formatExtractedNumber);
-      extractedNumbersFromData.push(formatExtractedNumber);
+      const replaceOne = line.replace('one', 1);
+      const replaceTwo = replaceOne.replace('two', 2);
+      const replaceThree = replaceTwo.replace('three', 3);
+      const replaceFour = replaceThree.replace('four', 4);
+      const replaceFive = replaceFour.replace('five', 5);
+      const replaceSix = replaceFive.replace('six', 6);
+      const replaceSeven = replaceSix.replace('seven', 7);
+      const replaceEight = replaceSeven.replace('eight', 8);
+      const replaceNine = replaceEight.replace('nine', 9);
+      const iterableArray = replaceNine.split('');
+      console.log(iterableArray);
+      for (let i = 0; i < iterableArray.length; i++) {
+        console.log(parseInt(iterableArray[i]));
+        if (parseInt(iterableArray[i]) === typeof Number) {
+          extractedNumbersFromData.push(parseInt(iterableArray[i]));
+        }
+      }
     });
+
+    console.log(extractedNumbersFromData);
 
     console.log(extractedNumbersFromData);
 
